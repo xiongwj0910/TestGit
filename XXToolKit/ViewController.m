df352af556b2242cf62f8d8196f187855fc3d845
wiki_0910 <xiongwenjie@126.com>
2016-10-23 23:04:38 +0800

    test002

--- XXToolKit/ViewController.m
+++ XXToolKit/ViewController.m
@@ -17,7 +17,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
-    NSLog(@"hhhhh");
+    NSLog(@"hhhhh-----ttttttt");
 }
 
 - (void)didReceiveMemoryWarning {
